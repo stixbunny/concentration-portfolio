08843e04b77caf2a3c8cf9e0681b9906f8077d48
Milenko Castillo <milenko.castillo@gmail.com>
2023-11-07 23:52:53 -0300

    api change

--- src/composables/useAnimals.js
+++ src/composables/useAnimals.js
@@ -2,20 +2,21 @@ import { fetchAnimals } from './fetchAnimals';
 
 export const useAnimals = async function () {
   const response = await fetchAnimals();
+  console.log(response)
   // receives a json object
-  if (!response.status) {
+  if (response.status && response.status === "success") {
     const animals = [];
-    response.entries.forEach((entry) => {
+    response.message.forEach((entry, i) => {
       animals.push({
-        name: entry.fields.image.title,
-        url: entry.fields.image.url,
+        name: entry.split('/breeds/')[1].split('/')[0] + `-${i}`,
+        url: entry,
       });
     });
     return animals;
   }
   // receives a rejected promise
   else {
-    console.log(response.error());
+    console.log(response.error);
     return null;
   }
 };
